3dd15e60a76849f4c0d79fb7730cb840c71dcb28
Cédric DEMONGIVERT <cedric.demongivert@gmail.com>
2018-09-20 10:45:59 -0400

    license

--- README.md
+++ README.md
@@ -5,4 +5,3 @@
 [![Build Status](https://jenkins.cedric-demongivert.com/buildStatus/icon?job=org.liara.collection/java-rest-collection/master)](https://jenkins.cedric-demongivert.com/job/org.liara.collection/job/java-rest-collection/job/master/)
 
 A Java library for handling REST collections.
-
--- src/test/groovy/org/liara/collection/jpa/JPAEntityCollectionSpecification.groovy
+++ src/test/groovy/org/liara/collection/jpa/JPAEntityCollectionSpecification.groovy
@@ -1,3 +1,25 @@
+/*
+ * Copyright (C) 2018 Cedric DEMONGIVERT <cedric.demongivert@gmail.com>
+ *
+ * Permission is hereby granted,  free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction,  including without limitation the rights
+ * to use,  copy, modify, merge,  publish,  distribute, sublicense,  and/or sell
+ * copies  of the  Software, and  to  permit persons  to  whom  the  Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The  above  copyright  notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE  SOFTWARE IS  PROVIDED  "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED,  INCLUDING  BUT  NOT LIMITED  TO THE  WARRANTIES  OF MERCHANTABILITY,
+ * FITNESS  FOR  A PARTICULAR  PURPOSE  AND  NONINFRINGEMENT. IN NO  EVENT SHALL
+ * THE  AUTHORS OR  COPYRIGHT  HOLDERS  BE  LIABLE FOR  ANY  CLAIM,  DAMAGES  OR
+ * OTHER  LIABILITY, WHETHER  IN  AN  ACTION  OF  CONTRACT,  TORT  OR  OTHERWISE,
+ * ARISING  FROM,  OUT  OF OR  IN  CONNECTION  WITH THE  SOFTWARE OR  THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package org.liara.collection.jpa
 
 import org.liara.collection.Specification
@@ -9,7 +31,6 @@ import org.liara.collection.operator.grouping.Group
 import org.liara.collection.operator.grouping.GroupableCollection
 import org.liara.collection.operator.ordering.Order
 import org.mockito.Mockito
-import org.mockito.internal.matchers.Or
 
 import javax.persistence.EntityManager
 import javax.persistence.TypedQuery
@@ -694,7 +715,7 @@ class JPAEntityCollectionSpecification extends Specification {
     ).apply(new JPAEntityCollection<>(manager, Object.class)) as JPAEntityCollection<Object>
 
     and: 'a group operator'
-    final Group group = Group.expression(":this.type");
+    final Group group = Group.expression(":this.type")
 
     when: 'we apply a grouping operator to the collection'
     final org.liara.collection.Collection result = group.apply(collection)
